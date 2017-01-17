39f009212bc35db13294b992257cede803ad2230
Michael Rapp <michael.rapp90@googlemail.com>
2017-01-17 04:03:25 +0100

    Added comments.

--- library/src/main/java/de/mrapp/android/tabswitcher/drawable/TabSwitcherDrawable.java
+++ library/src/main/java/de/mrapp/android/tabswitcher/drawable/TabSwitcherDrawable.java
@@ -1,3 +1,16 @@
+/*
+ * Copyright 2016 Michael Rapp
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package de.mrapp.android.tabswitcher.drawable;
 
 import android.content.Context;
@@ -7,9 +20,15 @@ import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
 
 import de.mrapp.android.tabswitcher.R;
+import de.mrapp.android.tabswitcher.TabSwitcher;
 
 /**
+ * A drawable, which allows to show the number of tabs, which are currently contained by a {@link
+ * TabSwitcher}. It must be registered at a {@link TabSwitcher} instance in order to keep the
+ * displayed count up to date.
+ *
  * @author Michael Rapp
+ * @since 1.0.0
  */
 public class TabSwitcherDrawable extends LayerDrawable {
 
