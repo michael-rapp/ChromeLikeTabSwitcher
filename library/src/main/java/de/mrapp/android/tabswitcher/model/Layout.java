b202aa36adaf6b53cf70d4eea4e0f3325d9fd741
Michael Rapp <michael.rapp90@googlemail.com>
2017-02-23 17:06:13 +0100

    Added the enum Layout.

---
+++ library/src/main/java/de/mrapp/android/tabswitcher/model/Layout.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2016 - 2017 Michael Rapp
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package de.mrapp.android.tabswitcher.model;
+
+import de.mrapp.android.tabswitcher.TabSwitcher;
+
+/**
+ * Contains all possible layouts of a {@link TabSwitcher}.
+ *
+ * @author Michael Rapp
+ * @since 1.0.0
+ */
+public enum Layout {
+
+    /**
+     * The layout, which is used on smartphones and phablet devices, when in portrait mode.
+     */
+    PHONE_PORTRAIT,
+
+    /**
+     * The layout, which is used on smartphones and phablet devices, when in landscape mode.
+     */
+    PHONE_LANDSCAPE
+
+}
