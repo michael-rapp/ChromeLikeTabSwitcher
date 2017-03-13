72f5866319624dcc7e293f73cb35b092c4fe3e4f
Michael Rapp <michael.rapp90@googlemail.com>
2017-03-14 00:56:58 +0100

    Added comments.

--- library/src/main/java/de/mrapp/android/tabswitcher/TabSwitcherDecorator.java
+++ library/src/main/java/de/mrapp/android/tabswitcher/TabSwitcherDecorator.java
@@ -22,16 +22,100 @@ import android.view.ViewGroup;
 
 import de.mrapp.android.util.view.AbstractViewHolderAdapter;
 
+/**
+ * An abstract base class for all decorators, which  are responsible for inflating views, which
+ * should be used to visualize the tabs of a {@link TabSwitcher}.
+ *
+ * @author Michael Rapp
+ * @since 1.0.0
+ */
 public abstract class TabSwitcherDecorator extends AbstractViewHolderAdapter {
 
+    /**
+     * The method which is invoked, when a view, which is used to visualize a tab, should be
+     * inflated.
+     *
+     * @param inflater
+     *         The inflater, which should be used to inflate the view, as an instance of the class
+     *         {@link LayoutInflater}. The inflater may not be null
+     * @param parent
+     *         The parent view of the view, which should be inflated, as an instance of the class
+     *         {@link ViewGroup} or null, if no parent view is available
+     * @param viewType
+     *         The view type of the tab, which should be visualized, as an {@link Integer} value
+     * @return The view, which has been inflated, as an instance of the class {@link View}. The view
+     * may not be null
+     */
+    @NonNull
+    public abstract View onInflateView(@NonNull final LayoutInflater inflater,
+                                       @Nullable final ViewGroup parent, final int viewType);
+
+    /**
+     * The method which is invoked, when the view, which is used to visualize a tab, should be
+     * shown, respectively when it should be refreshed. The purpose of this method is to customize
+     * the appearance of the view, which is used to visualize the corresponding tab, depending on
+     * its state and whether the tab switcher is currently shown, or not.
+     *
+     * @param context
+     *         The context, the tab switcher belongs to, as an instance of the class {@link
+     *         Context}. The context may not be null
+     * @param tabSwitcher
+     *         The tab switcher, whose tabs are visualized by the decorator, as an instance of the
+     *         type {@link TabSwitcher}. The tab switcher may not be null
+     * @param view
+     *         The view, which is used to visualize the tab, as an instance of the class {@link
+     *         View}. The view may not be null
+     * @param tab
+     *         The tab, which should be visualized, as an instance of the class {@link Tab}. The tab
+     *         may not be null
+     * @param viewType
+     *         The view type of the tab, which should be visualized, as an {@link Integer} value
+     */
+    public abstract void onShowTab(@NonNull final Context context,
+                                   @NonNull final TabSwitcher tabSwitcher, @NonNull final View view,
+                                   @NonNull final Tab tab, final int viewType);
+
+    /**
+     * Returns the view type, which corresponds to a specific tab. For each layout, which is
+     * inflated by the <code>onInflateView</code>-method, a distinct view type must be
+     * returned.
+     *
+     * @param tab
+     *         The tab, whose view type should be returned, as an instance of the class {@link Tab}.
+     *         The tab may not be null
+     * @return The view type, which corresponds to the given tab, as an {@link Integer} value
+     */
     public int getViewType(@NonNull final Tab tab) {
         return 0;
     }
 
+    /**
+     * Returns the number of view types, which are used by the decorator.
+     *
+     * @return The number of view types, which are used by the decorator, as an {@link Integer}
+     * value. The number of view types must correspond to the number of distinct values, which are
+     * returned by the <code>getViewType</code>-method
+     */
     public int getViewTypeCount() {
         return 1;
     }
 
+    /**
+     * The method, which is invoked by a {@link TabSwitcher} to inflate the view, which should be
+     * used to visualize a specific tab.
+     *
+     * @param inflater
+     *         The inflater, which should be used to inflate the view, as an instance of the class
+     *         {@link LayoutInflater}. The inflater may not be null
+     * @param parent
+     *         The parent view of the view, which should be inflated, as an instance of the class
+     *         {@link ViewGroup} or null, if no parent view is available
+     * @param tab
+     *         The tab, which should be visualized, as an instance of the class {@link Tab}. The tab
+     *         may not be null
+     * @return The view, which has been inflated, as an instance of the class {@link View}. The view
+     * may not be null
+     */
     @NonNull
     public final View inflateView(@NonNull final LayoutInflater inflater,
                                   @Nullable final ViewGroup parent, @NonNull final Tab tab) {
@@ -39,6 +123,24 @@ public abstract class TabSwitcherDecorator extends AbstractViewHolderAdapter {
         return onInflateView(inflater, parent, viewType);
     }
 
+    /**
+     * The method, which is invoked by a {@link TabSwitcher} to apply the decorator. It initializes
+     * the view holder pattern, which is provided by the decorator and then delegates the method
+     * call to the decorator's custom implementation of the method <code>onShowTab(...):void</code>.
+     *
+     * @param context
+     *         The context, the tab switcher belongs to, as an instance of the class {@link
+     *         Context}. The context may not be null
+     * @param tabSwitcher
+     *         The tab switcher, whose tabs are visualized by the decorator, as an instance of the
+     *         class {@link TabSwitcher}. The tab switcher may not be null
+     * @param view
+     *         The view, which is used to visualize the tab, as an instance of the class {@link
+     *         View}. The view may not be null
+     * @param tab
+     *         The tab, which should be visualized, as an instance of the class {@link Tab}. The tab
+     *         may not be null
+     */
     public final void applyDecorator(@NonNull final Context context,
                                      @NonNull final TabSwitcher tabSwitcher,
                                      @NonNull final View view, @NonNull final Tab tab) {
@@ -46,13 +148,5 @@ public abstract class TabSwitcherDecorator extends AbstractViewHolderAdapter {
         int viewType = getViewType(tab);
         onShowTab(context, tabSwitcher, view, tab, viewType);
     }
-
-    @NonNull
-    public abstract View onInflateView(@NonNull final LayoutInflater inflater,
-                                       @Nullable final ViewGroup parent, final int viewType);
-
-    public abstract void onShowTab(@NonNull final Context context,
-                                   @NonNull final TabSwitcher tabSwitcher, @NonNull final View view,
-                                   @NonNull final Tab tab, final int viewType);
 
 }
