5d94fae27d5ed6d4b5589e79722697d086b83371
Michael Rapp <michael.rapp90@googlemail.com>
2017-04-05 17:02:50 +0200

    Organized imports.

--- library/src/main/java/de/mrapp/android/tabswitcher/view/PreviewDataBinder.java
+++ library/src/main/java/de/mrapp/android/tabswitcher/view/PreviewDataBinder.java
@@ -15,9 +15,6 @@ package de.mrapp.android.tabswitcher.view;
 
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.Typeface;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.util.LruCache;
