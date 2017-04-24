835d42a69cbdbee00cc40c8dd1d1582c082b4307
Michael Rapp <michael.rapp90@googlemail.com>
2017-04-24 17:38:10 +0200

    Added the class TabletTabViewHolder.

---
+++ library/src/main/java/de/mrapp/android/tabswitcher/layout/tablet/TabletTabViewHolder.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2016 - 2017 Michael Rapp
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package de.mrapp.android.tabswitcher.layout.tablet;
+
+import de.mrapp.android.tabswitcher.TabSwitcher;
+import de.mrapp.android.tabswitcher.layout.AbstractTabViewHolder;
+
+/**
+ * A view holder, which allows to store references to the views, a tab of a {@link TabSwitcher}
+ * consists of, when using the tablet layout.
+ *
+ * @author Michael Rapp
+ * @since 1.0.0
+ */
+public class TabletTabViewHolder extends AbstractTabViewHolder {
+
+}
